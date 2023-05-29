78661980c4b2804054ef8229e6d1b57db5981114
vb968 <vb968g@gmail.com>
2023-05-29 10:33:51 +0700

    created the project

---
+++ app/src/main/java/com/b2b/rqst/ui/main/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.b2b.rqst.ui.main.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
