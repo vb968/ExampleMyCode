9b1ee17310241d7379dda7293923c24bb61db628
vb968 <vb968g@gmail.com>
2023-05-31 21:35:41 +0700

    Added RequestAdapter

--- app/src/main/java/com/b2b/rqst/ui/main/requests/RequestAdapter.kt
+++ app/src/main/java/com/b2b/rqst/ui/main/requests/RequestAdapter.kt
@@ -10,6 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.b2b.rqst.R
 import com.b2b.rqst.model.Request
 import java.text.SimpleDateFormat
 import java.time.ZonedDateTime
@@ -21,25 +22,25 @@ class RequestAdapter(private var ticketList: List<Request>, private val onClick:
 
     class ViewHolder(view: View, val onClick: (Request) -> Unit) : RecyclerView.ViewHolder(view) {
         private var currentTicket: Request? = null
-        val numberTicket: TextView = view.findViewById(R.id.number_ticket)
-        val titleText: TextView = view.findViewById(R.id.title_text)
-        val dateTicket: TextView = view.findViewById(R.id.date_ticket)
-        val typeTicket: TextView = view.findViewById(R.id.type_ticket)
-        val priceTicket: TextView = view.findViewById(R.id.price_ticket)
-        val prizeMoney: TextView = view.findViewById(R.id.prize_money)
-        val imageTicket: ImageView = view.findViewById(R.id.image_ticket)
-        val openGame: ConstraintLayout = view.findViewById(R.id.constraint_button_open)
+        val price: TextView = view.findViewById(R.id.price)
+        val requestNumber: TextView = view.findViewById(R.id.request_number)
+        val status: TextView = view.findViewById(R.id.status)
+        val chevronDown: TextView = view.findViewById(R.id.chevron_down)
+        val chevronUp: TextView = view.findViewById(R.id.chevron_up)
+        val row_form_0: ConstraintLayout = view.findViewById(R.id.row_form_0)
+        val row_form_1: ConstraintLayout = view.findViewById(R.id.row_form_1)
+        val row_form_2: ConstraintLayout = view.findViewById(R.id.row_form_2)
+        val row_form_3: ConstraintLayout = view.findViewById(R.id.row_form_3)
         val context: Context
         init {
             context = view.context
-            openGame.setOnClickListener {
+           /* openGame.setOnClickListener {
                 openGame.isClickable = false
-//                openGame.setBackgroundResource(R.drawable.grey_coral_oval)
 
                 currentTicket?.let {
                     onClick(it)
                 }
-            }
+            }*/
         }
         fun bind(ticket: Request) {
             currentTicket = ticket
@@ -47,13 +48,13 @@ class RequestAdapter(private var ticketList: List<Request>, private val onClick:
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.win_ticket, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.request, parent, false)
         return ViewHolder(view, onClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         if (ticketList.isEmpty()){return}
-        holder.numberTicket.text = holder.context.getString(R.string.grill_, ticketList[position].number.toString())
+       /* holder.numberTicket.text = holder.context.getString(R.string.grill_, ticketList[position].number.toString())
         holder.titleText.text = ticketList[position].group?.number
         holder.prizeMoney.text = ticketList[position].reward.toString()
         holder.typeTicket.text = ticketList[position].template.name
@@ -77,7 +78,7 @@ class RequestAdapter(private var ticketList: List<Request>, private val onClick:
             val bytes = android.util.Base64.decode(imageString, android.util.Base64.DEFAULT)
             val bitmap = BitmapFactory.decodeByteArray(bytes,0, bytes.size)
             holder.imageTicket.setImageBitmap(bitmap)
-        }
+        }*/
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/b2b/rqst/ui/main/requests/RequestsFragment.kt
+++ app/src/main/java/com/b2b/rqst/ui/main/requests/RequestsFragment.kt
@@ -1,5 +1,7 @@
 package com.b2b.rqst.ui.main.requests
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +9,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.b2b.rqst.databinding.FragmentRequestsBinding
+import com.b2b.rqst.model.Request
 
 class RequestsFragment : Fragment() {
 
@@ -21,15 +25,20 @@ class RequestsFragment : Fragment() {
         val dashboardViewModel = ViewModelProvider(this).get(RequestsViewModel::class.java)
 
         _binding = FragmentRequestsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+
+        val recyclerView: RecyclerView = binding.recyclerView
+        val requestAdapter = RequestAdapter { request -> adapterOnClick(request) }
+        recyclerView.adapter = requestAdapter
 
         /*val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }*/
-        return root
+        return binding.root
     }
+    private fun adapterOnClick(request: Request) {
 
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
