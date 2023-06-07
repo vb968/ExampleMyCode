727331fba4a5f52af0c227f5163174787f3c42c0
vb968 <vb968g@gmail.com>
2023-06-08 01:54:47 +0700

    Added AddRequestFragment

--- app/src/main/java/com/b2b/rqst/ui/main/addrequest/AddRequestFragment.kt
+++ app/src/main/java/com/b2b/rqst/ui/main/addrequest/AddRequestFragment.kt
@@ -23,10 +23,10 @@ class AddRequestFragment : Fragment() {
         _binding = FragmentAddRequestBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+       /* val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
         return root
     }
 
