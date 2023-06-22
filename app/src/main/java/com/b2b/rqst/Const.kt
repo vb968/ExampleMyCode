567bafaede833183567511b57cf68dd3c9f1e964
vb968 <vb968g@gmail.com>
2023-06-22 18:31:25 +0700

    Base url changed

--- app/src/main/java/com/b2b/rqst/Const.kt
+++ app/src/main/java/com/b2b/rqst/Const.kt
@@ -3,7 +3,7 @@ package com.b2b.rqst
 
 object Const {
 
-    const val BASE_URL_DEBUG = "https://b2b-request.pavlo.dev/api/"
-    const val BASE_URL_RELEASE = "https://b2b-request.pavlo.dev/api/"
+    const val BASE_URL_DEBUG = "https://b2brqst.com/api/"
+    const val BASE_URL_RELEASE = "https://b2brqst.com/api/"
     const val TOKEN_SAVE = "token_save"
 }
